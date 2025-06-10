112b6619b65c5b19cc288ae2545468acbbc5b4f4
TUsama <1716976420@qq.com>
2025-06-10 10:42:31 +0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -88,9 +88,9 @@ modstitch {
                     else -> ""
                 }
             )
-            findProperty("deps.forge")?.let {
-                put("target_forge", it as String)
-            }
+
+            put("target_forge", findProperty("deps.forge") as? String ?: "")
+
 
             put("fzzy_config_version", property("deps.fzzy_config_version") as String)
             put("common_networking_version", property("deps.common_networking_ingame_version") as String)
